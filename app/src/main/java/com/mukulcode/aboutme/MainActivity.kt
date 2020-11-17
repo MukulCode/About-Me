a480399261eed04afb49b7109db4baf8cf4c3fac
Mukul Kumar <mukul.2121@gmail.com>
2020-11-17 15:33:31 +0530

    added a button and nickname field

--- app/src/main/java/com/mukulcode/aboutme/MainActivity.kt
+++ app/src/main/java/com/mukulcode/aboutme/MainActivity.kt
@@ -1,11 +1,38 @@
  package com.mukulcode.aboutme
 
+import android.content.Context
+import android.inputmethodservice.InputMethodService
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val button = findViewById<Button>(R.id.done_button).setOnClickListener{
+            addNickName(it)
+        }
+    }
+
+
+    private fun addNickName(view : View){
+        val editText = findViewById<EditText>(R.id.my_nickname_edit)
+        val textView = findViewById<TextView>(R.id.nickname_text)
+
+        textView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        textView.visibility = View.VISIBLE
+
+        //Hiding the keyboard automatically
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
